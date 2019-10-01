e49e4490cd18d5c334fea4a40d85e34b0e20b0a3
Dom Tunstill <datunstill@gmail.com>
2019-10-01 14:48:47 +0100

    Setup RSpec, gemfile and sinatra

---
+++ app.rb
@@ -0,0 +1,13 @@
+require 'sinatra'
+
+# app.rb
+require 'sinatra/base'
+
+class Battle < Sinatra::Base
+  # ... app code here ...
+  get '/' do
+    "Hello World"
+  end
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
